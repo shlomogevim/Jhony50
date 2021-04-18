c3d6cfe5f5ac450284f6778a39221a7c759928d6
shlomo <shlomo.gevim@gmail.com>
2021-04-18 07:17:23 +0300

    befor last per. in this section

--- app/src/main/java/com/sg/jhony50/MainActivity.kt
+++ app/src/main/java/com/sg/jhony50/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import com.sg.jhony50.AddThoughtActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var selectCategory: String
     lateinit var thoughtsAdapter: ThoughtsAdapter
     var thoughts = ArrayList<Thought>()
-    val thoughtCollectionRef=FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
+    val thoughtCollectionRef = FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
+    lateinit var thoughtsListener: ListenerRegistration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +36,11 @@ class MainActivity : AppCompatActivity() {
         val layoutManger = LinearLayoutManager(this)
         thoughtListView.layoutManager = layoutManger
 
-        thoughtCollectionRef.get()
+        /*thoughtCollectionRef.get()
             .addOnSuccessListener { snapshot ->
-                for (document in snapshot.documents) {
-                    val data =document.data
-                    Log.i(TAG,"${data}")
+               *//* for (document in snapshot.documents) {
+                    val data = document.data
+                    Log.i(TAG, "${data}")
                     if (data != null) {
                         val name = data[USERNAME] as String
                         val timestamp = data[TIMESTAMP] as Timestamp
@@ -55,16 +58,92 @@ class MainActivity : AppCompatActivity() {
                         )
                         thoughts.add(newThought)
                     }
- thoughtsAdapter.notifyDataSetChanged()
-                    Log.i(TAG,"end")
+                    thoughtsAdapter.notifyDataSetChanged()
+                }*//*
+
+            }.addOnFailureListener { e ->
+
+                Log.i(TAG, "Canot retrive data because :${e.localizedMessage}")
+
+            }*/
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setListener()
+    }
+
+    fun setListener() {
+        if (selectCategory == POPULAR) {
+            thoughtsListener = thoughtCollectionRef
+                .orderBy(NUM_LIKES, Query.Direction.DESCENDING)
+                .addSnapshotListener(this) { snapshot, exception ->
+                    if (exception != null) {
+                        Log.i(TAG, "cant retrive data :${exception.localizedMessage}")
+                    }
+                    if (snapshot != null) {
+                        thoughts.clear()
+                        for (document in snapshot.documents) {
+                            val data = document.data
+                            if (data != null) {
+                                val name = data[USERNAME] as String
+                                val timestamp = data[TIMESTAMP] as Timestamp
+                                var thoghtTxt = "ff"
+                                if (data[THOUGHT_TXT] != null) {
+                                    thoghtTxt = data[THOUGHT_TXT] as String
+                                }
+                                val numLikes = data[NUM_LIKES] as Long
+                                Log.i("message", "numLikes=$numLikes")
+                                val numComments = data[NUM_COMMENTS] as Long
+                                val documentId = document.id
+                                val newThought = Thought(
+                                    name, timestamp, thoghtTxt, numLikes.toInt(),
+                                    numComments.toInt(), documentId
+                                )
+
+                                thoughts.add(newThought)
+                            }
+                            thoughtsAdapter.notifyDataSetChanged()
+                        }
+                    }
                 }
 
-            }.addOnFailureListener { e->
+        } else {
+            thoughtsListener = thoughtCollectionRef
+                .orderBy(TIMESTAMP, Query.Direction.DESCENDING)
+                .whereEqualTo(CATEGORY,selectCategory)
+                .addSnapshotListener(this) { snapshot, exception ->
+                    if (exception != null) {
+                        Log.i(TAG, "cant retrive data :${exception.localizedMessage}")
+                    }
+                    if (snapshot != null) {
+                        thoughts.clear()
+                        for (document in snapshot.documents) {
+                            val data = document.data
+                            if (data != null) {
+                                val name = data[USERNAME] as String
+                                val timestamp = data[TIMESTAMP] as Timestamp
+                                var thoghtTxt = "ff"
+                                if (data[THOUGHT_TXT] != null) {
+                                    thoghtTxt = data[THOUGHT_TXT] as String
+                                }
+                                val numLikes = data[NUM_LIKES] as Long
+                                Log.i("message", "numLikes=$numLikes")
+                                val numComments = data[NUM_COMMENTS] as Long
+                                val documentId = document.id
+                                val newThought = Thought(
+                                    name, timestamp, thoghtTxt, numLikes.toInt(),
+                                    numComments.toInt(), documentId
+                                )
 
-                Log.i(TAG,"Canot retrive data because :${e.localizedMessage}")
-
-            }
-
+                                thoughts.add(newThought)
+                            }
+                            thoughtsAdapter.notifyDataSetChanged()
+                        }
+                    }
+                }
+        }
     }
 
     fun mainFunnyOnClick(view: View) {
@@ -73,6 +152,8 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = false
         selectCategory = FUNNY
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainSeriousOnclicked(view: View) {
@@ -81,6 +162,8 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = false
         selectCategory = SERIOUS
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainCreazyOnclicked(view: View) {
@@ -89,6 +172,8 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = true
         mainPopularBtn.isChecked = false
         selectCategory = CRAZY
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainPopularOnclicked(view: View) {
@@ -97,5 +182,7 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = true
         selectCategory = POPULAR
+        thoughtsListener.remove()
+        setListener()
     }
 }
